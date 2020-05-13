27beda7ed054f72dba39a0d9b05d40aea83e85e8
Chris Knorowski <chris.knorowski@sensiml.com>
2020-05-12 23:24:35 -0700

    switch to using a json file as input

--- tensorflow/lite/micro/examples/cwrapper/micro_api_benchmark.h
+++ tensorflow/lite/micro/examples/cwrapper/micro_api_benchmark.h
@@ -2,91 +2,82 @@
 #ifndef TENSORFLOW_LITE_MICRO_API_BENCHMARK_H_
 #define TENSORFLOW_LITE_MICRO_API_BENCHMARK_H_
 
-
-#include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/examples/cwrapper/micro_api.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 
+static int test_allocate(char* tflite_buffer, size_t arena_size) {
+  int ret;
 
+  uint8_t* tensor_arena = (uint8_t*)malloc(arena_size);
+  if (!tensor_arena) {
+    return malloc_failed;
+  }
 
-static int test_allocate(char *tflite_buffer, size_t arena_size) {
+  ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
 
-    int ret;
+  free(tensor_arena);
 
-    uint8_t *tensor_arena = (uint8_t*)malloc(arena_size);
-    if (!tensor_arena) {
-        return malloc_failed;
-    }
+  return ret;
+}
 
-    ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
+static int test_invoke(char* tflite_buffer, size_t arena_size,
+                       float* input_data, int num_inputs, float* results,
+                       int num_outputs) {
+  int ret;
 
-    free(tensor_arena);
+  uint8_t* tensor_arena = (uint8_t*)malloc(arena_size);
 
+  if (!tensor_arena) {
+    return malloc_failed;
+  }
+
+  ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
+
+  if (ret != success) {
     return ret;
+  }
+
+  ret = tf_micro_model_invoke(input_data, num_inputs, results, num_outputs)
+
+      free(tensor_arena);
+
+  return ret;
 }
 
-static int test_invoke(char *tflite_buffer, size_t arena_size, float* input_data, int num_inputs, float* results,
-                           int num_outputs)) {
+static int find_arena_size(char* tflite_buffer, int* arena_size, size_t a_low,
+                           size_t a_high) {
+  size_t low = a_low;
+  size_t high = a_high;
+  size_t curr = low + ((high - low) / 2);
+  size_t last_success = high;
+  size_t steps = 0;
 
-    int ret;
+  while (steps < 100) {
+    ++steps;
+    int r = test_allocate(tflite_buffer, curr);
 
-    uint8_t *tensor_arena = (uint8_t*)malloc(arena_size);
-
-    if (!tensor_arena) {
-        return malloc_failed;
+    if (r != success) {
+      last_success = curr;
     }
 
-    ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
-
-    if (ret != success)
-    {
-        return ret;
+    if (r == invoke_failed) {  // too low
+      low = curr;
+      curr = low + ((high - low) / 2);
+    } else {
+      high = curr;
+      curr = low + ((high - low) / 2);
     }
 
-    ret = tf_micro_model_invoke(input_data, num_inputs, results, num_outputs)
+    if (low == curr || high == curr) {
+      break;
+    }
+  }
 
-    free(tensor_arena);
+  if (last_success == a_high || steps >= 99) {
+    return allocate_failed;
+  }
 
-    return ret;
+  return last_success;
 }
 
-
-static int find_arena_size(char *tflite_buffer, int * arena_size, size_t a_low, size_t a_high) {
-
-
-    size_t low = a_low;
-    size_t high = a_high;
-    size_t curr = low + ((high - low) / 2);
-    size_t last_success = high;
-    size_t steps = 0;
-
-    while (steps < 100) {
-        ++steps;
-        int r = test_allocate(tflite_buffer, curr);
-
-        if (r != success) {
-            last_success = curr;
-        }
-
-        if (r == invoke_failed) { // too low
-            low = curr;
-            curr = low + ((high - low) / 2);
-        }
-        else {
-            high = curr;
-            curr = low + ((high - low) / 2);
-        }
-
-        if (low == curr || high == curr) {
-            break;
-        }
-
-    }
-
-    if (last_success == a_high || steps >= 99) {
-        return allocate_failed;
-    }
-
-    return last_success;
-}
-
-#endif //TENSORFLOW_LITE_MICRO_API_BENCHMARK_H_
+#endif  // TENSORFLOW_LITE_MICRO_API_BENCHMARK_H_
