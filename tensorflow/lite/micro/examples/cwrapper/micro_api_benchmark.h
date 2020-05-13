43875fb83c8ec343cdbfcb98dd11063321774792
Chris Knorowski <chris.knorowski@sensiml.com>
2020-05-13 09:18:33 -0700

    modify some more stuff

--- tensorflow/lite/micro/examples/cwrapper/micro_api_benchmark.h
+++ tensorflow/lite/micro/examples/cwrapper/micro_api_benchmark.h
@@ -5,7 +5,8 @@
 #include "tensorflow/lite/micro/examples/cwrapper/micro_api.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
-static int test_allocate(const unsigned char* tflite_buffer, size_t arena_size) {
+static bool set_static=false;
+static int test_allocate(const unsigned char* tflite_buffer, int arena_size) {
   int ret;
 
   uint8_t* tensor_arena = (uint8_t*)malloc(arena_size);
@@ -13,54 +14,52 @@ static int test_allocate(const unsigned char* tflite_buffer, size_t arena_size) 
     return malloc_failed;
   }
 
-  ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
+  ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size, set_static);
 
   free(tensor_arena);
 
   return ret;
 }
 
-static int test_invoke(const unsigned char* tflite_buffer, size_t arena_size,
+static int test_invoke(const unsigned char* tflite_buffer, uint8_t* tensor_arena,  int arena_size,
                        float* input_data, int num_inputs, float* results,
                        int num_outputs) {
   int ret;
 
-  uint8_t* tensor_arena = (uint8_t*)malloc(arena_size);
 
-  if (!tensor_arena) {
-    return malloc_failed;
-  }
+  if (set_static == false)
+    ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size, true);
 
-  ret = tf_micro_model_setup(tflite_buffer, tensor_arena, arena_size);
-
-  if (ret != success) {
-    return ret;
-  }
+    set_static=true;
+    
+    if (ret != success) {
+      printf("MODEL FAILED TO BE SET UP %d\n", ret);
+      return ret;
+    }
 
   ret = tf_micro_model_invoke(input_data, num_inputs, results, num_outputs);
-
-      free(tensor_arena);
 
   return ret;
 }
 
-static int find_arena_size(const unsigned char* tflite_buffer, int* arena_size, size_t a_low,
+static int find_arena_size(const unsigned char* tflite_buffer, int* arena_size_p, size_t a_low,
                            size_t a_high) {
-  size_t low = a_low;
-  size_t high = a_high;
-  size_t curr = low + ((high - low) / 2);
-  size_t last_success = high;
-  size_t steps = 0;
+  int low = a_low;
+  int high = a_high;
+  int curr = low + ((high - low) / 2);
+  int last_success = high;
+  int steps = 0;
 
   while (steps < 100) {
     ++steps;
     int r = test_allocate(tflite_buffer, curr);
+    printf("%d %d %d\n", r, last_success, curr);
 
-    if (r != success) {
+    if (r == success) {
       last_success = curr;
     }
 
-    if (r == invoke_failed) {  // too low
+    if (r == allocate_failed) {  // too low
       low = curr;
       curr = low + ((high - low) / 2);
     } else {
@@ -71,13 +70,16 @@ static int find_arena_size(const unsigned char* tflite_buffer, int* arena_size, 
     if (low == curr || high == curr) {
       break;
     }
+
+    if (last_success == a_high || steps >= 99) {
+      return allocate_failed;
+    }
+
   }
 
-  if (last_success == a_high || steps >= 99) {
-    return allocate_failed;
-  }
+  *arena_size_p = last_success;
 
-  return last_success;
+  return success;
 }
 
 #endif  // TENSORFLOW_LITE_MICRO_API_BENCHMARK_H_
