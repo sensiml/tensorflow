e96d86e2ee4ca95c19a0ac205f1d271c3a699886
Chris Knorowski <chris.knorowski@sensiml.com>
2020-05-18 11:40:31 -0700

    update lds to fit with renode image

--- tensorflow/lite/micro/examples/model_tester/main.cc
+++ tensorflow/lite/micro/examples/model_tester/main.cc
@@ -2,7 +2,7 @@
 #include "tensorflow/lite/micro/examples/model_tester/model.h"
 #include "tensorflow/lite/micro/examples/model_tester/test_data.h"
 
-#define MAX_TENSOR_ARENA_SIZE 7 * 1024
+#define MAX_TENSOR_ARENA_SIZE 10 * 1024
 #define MIN_TENSOR_ARENA_SIZE 2 * 1024
 #define ARENA_TOLERANCE  1024
 
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
                         ARENA_TOLERANCE);
 
   if (ret == success) {
-    printf("arena_size is %d\n", arena_size);
+    printf("Approximate Arena Size is %d\n", arena_size);
   }
 
   else if (ret == version_unspported) {
@@ -37,8 +37,6 @@ int main(int argc, char** argv) {
 
   if (ret == success) 
   {
-
-    printf("TEST INVOKE\n");
     for (int index = 0; index < TEST_DATA_LENGTH; index++) {
       for (int i = 0; i < MODEL_OUTPUTS; i++) {
         results[i] = 0.0;
